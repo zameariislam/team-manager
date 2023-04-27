e10440312fa10e47d4afef2203da747ce9d9a6cb
zameariislam <zameariislam@gmail.com>
2023-04-27 15:27:09 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+import './App.css';
+import Login from './pages/Login';
+
+function App() {
+  return (
+    <div >
+      <Login />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Login.js
@@ -0,0 +1,66 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div className="grid place-items-center h-screen bg-[#F9FAFB">
+            <div
+                className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8"
+            >
+                <div className="max-w-md w-full space-y-8">
+                    <div>
+                       
+                        <h2
+                            className="mt-6 text-center text-3xl font-extrabold text-gray-900"
+                        >
+                            Sign in to your account
+                        </h2>
+                    </div>
+                    <form className="mt-8 space-y-6" >
+                        
+                        <div className="rounded-md shadow-sm -space-y-px">
+                            <div>
+                                <label htmlFor="email-address" className="sr-only"
+                                    >Email address</label
+                                >
+                                <input
+                                    id="email-address"
+                                    name="email"
+                                    type="email"
+                                    autocomplete="email"
+                                    required
+                                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-violet-500 focus:border-violet-500 focus:z-10 sm:text-sm"
+                                    placeholder="Email address"
+                                />
+                            </div>
+                            <div>
+                                <label htmlFor="password" className="sr-only"
+                                    >Password</label
+                                >
+                                <input
+                                    id="password"
+                                    name="password"
+                                    type="password"
+                                    autocomplete="current-password"
+                                    required
+                                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-violet-500 focus:border-violet-500 focus:z-10 sm:text-sm"
+                                    placeholder="Password"
+                                />
+                            </div>
+                        </div>
+
+                        <div>
+                            <button
+                                type="submit"
+                                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-violet-600 hover:bg-violet-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-violet-500"
+                            >
+                                Sign in
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
